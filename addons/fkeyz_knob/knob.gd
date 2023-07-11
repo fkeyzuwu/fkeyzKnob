ec8615cbef045033af9a7de8c3572ef206e0b6d0
fkeyz <fkeyzuwu@gmail.com>
2023-07-11 10:11:35 +0300

    added better editor support for start_degrees

--- addons/fkeyz_knob/knob.gd
+++ addons/fkeyz_knob/knob.gd
@@ -2,15 +2,17 @@
 ##
 ## In order to use this script, please use knob.tscn. Using the knob.gd script alone will not work.[br][br]
 ## In case you want to change the knobs' default art, go inside the knob.tscn and change the sprites of the [code]Sprite2D[/code] nodes.
-## You might also need to resize them and scale them differently, and update the main "Knob" [code]Control[/code] nodes' [code]Custom Minimum Size[/code] in order to be able to use it correctly in conjunction with other UI elements in your scene.
+## You might also need to resize them and scale them differently, aswell as update the [code]Custom Minimum Size[/code] on the "Knob" [code]Control[/code] node in order to be able to use it correctly in conjunction with other UI elements in your scene.
+@tool
 class_name Knob extends Control
-
-
 
 ## How fast the dial moves with the mouse movement.
 @export_range(0.1, 2.0, 0.1) var dial_sensitivity := 1.0
 ## The rotation degrees the dial will start at.
-@export_range(-180.0, 180.0, 0.1, "degrees") var start_degrees := -130.0
+@export_range(-180.0, 180.0, 0.1, "degrees") var start_degrees := -130.0:
+	set(value):
+		start_degrees = clampf(value, minimum_degrees, maximum_degrees)
+		_dial.rotation_degrees = start_degrees
 ## The minimum rotation degrees the dial can be at.
 @export_range(-180.0, 180.0, 0.1, "degrees") var minimum_degrees := -130.0
 ## The maxiumum rotation degrees the dial can be at.
@@ -35,7 +37,6 @@ func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) 
 			_dragging = true
 			_mouse_position_start = Vector2(0, event.position.y)
 			_rotation_degrees_start = _dial.rotation_degrees
-			
 
 func _physics_process(delta: float) -> void:
 	if _dragging:
